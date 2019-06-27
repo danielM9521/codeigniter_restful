07ffad6d2f808c7a537ebd45f42f1a0a2243503f
CDS-SA-2011-018 <CDS-SA-2011-018@CDS-SA-2011-018>
2019-06-27 08:10:55 -0600

    api rest finalizada

---
+++ sql/db_api_restful_php.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS db_api_restful_php;
+CREATE DATABASE db_api_restful_php;
+USE db_api_restful_php;
+
+CREATE TABLE usuario(
+  id_usuario int(11) primary key auto_increment not null,
+  nombre varchar(50) not null,
+  apellido varchar(50) not null
+);
+INSERT INTO usuario (id_usuario, nombre, apellido) VALUES
+(1, 'Esteban', 'Quito'),
+(2, 'Daniel', 'Murillo'),
+(3, 'Adolf', 'Hitler');
